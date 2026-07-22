6fb4a258c5062777406f97d2203daf472cc69ec6
Raws Tourix <104128208+RawsTourix@users.noreply.github.com>
2026-07-22 17:37:36 +0300

    v0.4-file-artifacts: export unified ingress API

---
+++ src/ingress/__init__.py
@@ -0,0 +1,65 @@
+"""Public API for transport-neutral file ingress."""
+
+from .config import IngressConfigType
+from .factory import IngressServices, create_ingress_services
+from .models import (
+    ClientAttachmentLocator,
+    ClientConversationRef,
+    ClientIngressEvent,
+    ClientInputEnvelope,
+    ClientResponseRoute,
+    ClientSenderRef,
+    CommittedInputBatch,
+    IngressAttachmentSlot,
+    IngressTextPart,
+    InputAdmissionMode,
+    InputAttachmentPart,
+    InputAttachmentState,
+    InputBatchDraft,
+    InputBatchDraftState,
+    InputGroupingMode,
+    InputSubmissionResult,
+    is_ingress_event_id,
+    is_input_batch_id,
+    new_ingress_event_id,
+    new_input_batch_id,
+)
+from .service import ArtifactIngressService, IngressValidationError
+from .store import (
+    FileSystemIngressEventStore,
+    FileSystemInputBatchStore,
+    IngressConflictError,
+    IngressNotFoundError,
+)
+
+__all__ = [
+    "ArtifactIngressService",
+    "ClientAttachmentLocator",
+    "ClientConversationRef",
+    "ClientIngressEvent",
+    "ClientInputEnvelope",
+    "ClientResponseRoute",
+    "ClientSenderRef",
+    "CommittedInputBatch",
+    "FileSystemIngressEventStore",
+    "FileSystemInputBatchStore",
+    "IngressAttachmentSlot",
+    "IngressConfigType",
+    "IngressConflictError",
+    "IngressNotFoundError",
+    "IngressServices",
+    "IngressTextPart",
+    "IngressValidationError",
+    "InputAdmissionMode",
+    "InputAttachmentPart",
+    "InputAttachmentState",
+    "InputBatchDraft",
+    "InputBatchDraftState",
+    "InputGroupingMode",
+    "InputSubmissionResult",
+    "create_ingress_services",
+    "is_ingress_event_id",
+    "is_input_batch_id",
+    "new_ingress_event_id",
+    "new_input_batch_id",
+]
