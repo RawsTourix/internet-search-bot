4bb22e9395474ef76711abf91a9b39cb85c1d357
Raws Tourix <104128208+RawsTourix@users.noreply.github.com>
2026-08-01 16:41:06 +0300

    test(config): enforce resolvable env and registered sections

--- tests/test_artifact_configuration_examples.py
+++ tests/test_artifact_configuration_examples.py
@@ -2,14 +2,24 @@ import unittest
 
 from scripts.audit_configuration_examples import (
     missing_config_example_fields,
+    missing_config_example_sections,
     missing_environment_example_keys,
+    unregistered_config_sections,
+    unresolved_environment_reads,
     validate_config_example_values,
 )
 
 
 class ConfigurationExampleCoverageTests(unittest.TestCase):
+    def test_environment_reads_are_statically_auditable(self):
+        self.assertEqual(unresolved_environment_reads(), [])
+
     def test_env_example_covers_all_production_environment_reads(self):
         self.assertEqual(missing_environment_example_keys(), [])
+
+    def test_mcp_config_example_covers_all_root_sections(self):
+        self.assertEqual(missing_config_example_sections(), [])
+        self.assertEqual(unregistered_config_sections(), [])
 
     def test_mcp_config_example_covers_every_model_field(self):
         self.assertEqual(missing_config_example_fields(), {})
